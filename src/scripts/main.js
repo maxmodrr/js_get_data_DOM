27fce2c12c098c4ea1981113b388e01fd9eeff33
Maxym Modrytskyi <maxmodrr@gmail.com>
2024-10-17 02:46:46 +0300

    results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+const bodyElement = document.body;
+
+const population = [...bodyElement.querySelectorAll('.population')];
+const textTotal = bodyElement.querySelector('.total-population');
+const textAverage = bodyElement.querySelector('.average-population');
+
+const value = population.map((elem) => +elem.textContent.replaceAll(',', ''));
+
+const total = value
+  .reduce((sum, elem) => sum + elem, 0)
+  .toLocaleString('en-Us');
+
+const average = Math.round(total / value.length).toLocaleString('en-Us');
+
+textTotal.textContent = total;
+textAverage.textContent = average;
